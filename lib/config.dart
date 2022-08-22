60f59276b59c98626014439f1c408609569e1ebf
Moutaz Debbaneh <matazdev@gmail.com>
2022-08-22 17:27:05 +0300

    Edited gitignore

--- lib/config.dart
+++
@@ -1,7 +0,0 @@
-class Config {
-  static const String serverURL = 'http://10.0.2.2:3000';
-  static const String googleAPIKey = 'AIzaSyAplSjBOLE8r5RHIwr5Q996PE-qlVjoWIY';
-  static const String graphHopperAPIKey =
-      '0f7290b8-639f-43ca-814d-31c07a635b73';
-  static const Duration requestTimeoutDuration = Duration(seconds: 20);
-}
